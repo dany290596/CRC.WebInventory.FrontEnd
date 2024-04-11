396e61f388631661a6a9a1d8ca4f63e3ec8796fc
Daniel Benito Rosales <danielbr.estatus@gmail.com>
2024-04-11 17:11:35 -0600

    Se agrega un nuevo gif

--- ZebraRFIDXamarinDemo/ZebraRFIDXamarinDemo.Android/Properties/AssemblyInfo.cs
+++ ZebraRFIDXamarinDemo/ZebraRFIDXamarinDemo.Android/Properties/AssemblyInfo.cs
@@ -31,9 +31,9 @@ using Android.App;
 
 // Add some common permissions, these can be removed if not needed
 [assembly: UsesPermission(Android.Manifest.Permission.Internet)]
-[assembly: UsesPermission(Android.Manifest.Permission.WriteExternalStorage)]
-[assembly: UsesPermission(Android.Manifest.Permission.ReadExternalStorage)]
-[assembly: UsesPermission(Android.Manifest.Permission.ManageExternalStorage)]
+// [assembly: UsesPermission(Android.Manifest.Permission.WriteExternalStorage)]
+// [assembly: UsesPermission(Android.Manifest.Permission.ReadExternalStorage)]
+// [assembly: UsesPermission(Android.Manifest.Permission.ManageExternalStorage)]
 
 #if DEBUG
 [assembly: Application(Debuggable=true)]
